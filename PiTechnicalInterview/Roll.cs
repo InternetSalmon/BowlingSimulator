4d52d2be0e9b7520d27fa8d7917e874459e4cbb0
Tim Kalinowski <Tim.Kalinowski@elenium.com>
2021-12-08 10:38:05 +1100

    Added tests for roll class
    Refs: 0

---
+++ PiTechnicalInterview/Roll.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PiTechnicalInterview
+{
+    public class Roll
+    {
+        public int PinsKnocked { get; private set; }
+
+        public Roll(int pinsKnocked)
+        {
+            if (pinsKnocked > 10 || pinsKnocked < 0)
+                throw new ArgumentException("Pins knocked greater than 10");
+            PinsKnocked = pinsKnocked;
+        }
+    }
+}
---
+++ PiTechnicalInterviewTests/RollTests.cs
@@ -0,0 +1,28 @@
+﻿using PiTechnicalInterview;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace PiTechnicalInterviewTests
+{
+    public class RollTests
+    {
+        [Fact]
+        public void Roll_ValidRoll()
+        {
+            var roll = new Roll(10);
+            Assert.Equal(10, roll.PinsKnocked);
+        }
+
+        [Theory]
+        [InlineData(11)]
+        [InlineData(-1)]
+        public void Roll_InvalidRoll(int pinsKnocked)
+        {
+            Assert.Throws<ArgumentException>(() => {
+                new Roll(pinsKnocked);
+            });
+        }
+    }
+}
